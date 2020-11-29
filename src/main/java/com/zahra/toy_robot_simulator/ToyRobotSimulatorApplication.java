64bcc86fc993916f6ac4ea8059266d7571267f6e
Zahra <zahramousavi7090@gmail.com>
2020-11-29 12:49:35 +0330

    initial spring boot toy robot project

---
+++ src/main/java/com/zahra/toy_robot_simulator/ToyRobotSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.zahra.toy_robot_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyRobotSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyRobotSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zahra/toy_robot_simulator/ToyRobotSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zahra.toy_robot_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyRobotSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
